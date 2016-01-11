82d61d8982831ed010e4c27c69127efcbda2d489
Taylor Huston <taylor.a.huston@gmail.com>
2016-01-10 23:04:58 -0700

    Finish up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
 
 User.destroy_all
+TodoList.destroy_all
 
-User.create! [
-  {username: "Fiorina", password_digest: "pass"},
-  {username: "Trump", password_digest: "pass"},
-  {username: "Carson", password_digest: "pass"},
-  {username: "Clinton", password_digest: "pass"}
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
+
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
   ]
+end
 
-user = User.find_by(username: "Fiorina")
-user.profile = Profile.create!({first_name: "Carly", last_name: "Fiorina", birth_year: 1954, gender: "female"})
+users = User.create! [
+  { username: "jim", password: "abc123" },
+  { username: "rich", password: "123abc" }
+]
 
-user = User.find_by(username: "Trump")
-user.profile = Profile.create!({first_name: "Donald", last_name: "Trump", birth_year: 1946, gender: "male"})
-
-user = User.find_by(username: "Carson")
-user.profile = Profile.create!({first_name: "Ben", last_name: "Carson", birth_year: 1951, gender: "male"})
-
-user = User.find_by(username: "Clinton")
-user.profile = Profile.create!({first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female"})
-
-users = User.all.to_a
-
-users.each do |user|
-  user.todo_lists << TodoList.create!({list_name: "A List", list_due_date: (Date.today+1.year)})
-  
-  5.times do
-    user.todo_lists.first.todo_items << TodoItem.create({title: "An Item", description: "Description", due_date: (Date.today+1.year)})
-  end
-    
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
 end
